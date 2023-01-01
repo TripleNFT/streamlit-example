385732e94028012ca53a4b37336540ab0751f73a
TripleNFT <98217095+TripleNFT@users.noreply.github.com>
2023-01-01 13:29:25 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,16 @@
-import subprocess
 import streamlit as st
 import requests
-from bs4 import BeautifulSoup
 
-def install_dependencies(package_file):
-    with open(package_file, "r") as f:
-        packages = f.readlines()
-        for package in packages:
-            package = package.strip()
-            subprocess.run(["pip", "install", package])
+URL = "https://www.homedepot.ca/product/frigidaire-gallery-30-inch-5-4-cu-ft-front-control-slide-in-electric-range-with-air-fry-in-stainless-steel/1001318565"
 
-install_dependencies("Packages.txt")
+page = requests.get(URL)
 
-def get_price():
-    URL = "https://www.homedepot.ca/product/frigidaire-gallery-30-inch-5-4-cu-ft-front-control-slide-in-electric-range-with-air-fry-in-stainless-steel/1001318565"
+html = page.content
 
-    page = requests.get(URL)
+start_index = html.index('<span class="price">')
+end_index = html.index('</span>', start_index)
 
-    soup = BeautifulSoup(page.content, 'html.parser')
-
-    price = soup.find('span', class_='price').get_text()
-
-    return price
+price = html[start_index:end_index].split('>')[-1]
 
 st.title("Frigidaire Range Price")
-st.write("The price of the Frigidaire range is: $" + get_price())
+st.write("The price of the Frigidaire range is: $" + price)
