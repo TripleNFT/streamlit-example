18999c5aaf0a563e0c66be826028f2295b127cb6
TripleNFT <98217095+TripleNFT@users.noreply.github.com>
2023-01-01 12:51:58 -0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,13 +1,16 @@
+import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 
-URL = "https://www.homedepot.ca/product/frigidaire-gallery-30-inch-5-4-cu-ft-front-control-slide-in-electric-range-with-air-fry-in-stainless-steel/1001318565"
+def get_price():
+    URL = "https://www.homedepot.ca/product/frigidaire-gallery-30-inch-5-4-cu-ft-front-control-slide-in-electric-range-with-air-fry-in-stainless-steel/1001318565"
 
-page = requests.get(URL)
+    page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, 'html.parser')
 
-price = soup.find('span', class_='price').get_text()
+    price = soup.find('span', class_='price').get_text()
 
-print("The price of the Frigidaire range is: $" + price)
+    return price
 
+st.write("The price of the Frigidaire range is: $" + get_price())
